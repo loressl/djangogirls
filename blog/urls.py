21a88734a327ad9ebb3de4141d3722c87707e33f
loressl <loressl89@gmail.com>
2019-08-09 18:01:55 -0300

    Adicionado formulário

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    #path('post/', include('blog.urls')),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
